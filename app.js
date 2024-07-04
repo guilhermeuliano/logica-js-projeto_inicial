796d826e55fbcf2cb7e839ea4ecac9c1edc70ee3
guilhermeuliano <guilherme_uliano@hotmail.com>
2024-07-04 19:32:13 -0300

    aula3

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-alert("Jogo do número secreto!");
+alert('Jogo do número secreto!');
 
 let numeroSecreto = 5;
+let numeroEscolhido;
+let tentativas = 1;
 
-let numeroEscolhido = prompt("Escolha um número de 1 á 10: ");
+console.log(`Número secreto: ${numeroSecreto}`);
 
-if (numeroEscolhido == numeroSecreto) {
-  alert("Você acertou");
+while (numeroEscolhido != numeroSecreto) {
+  numeroEscolhido = prompt('Escolha um número de 1 á 10: ');
+
+  if (numeroEscolhido == numeroSecreto) {
+    alert(
+      `Você acertou! Número Secreto: ${numeroSecreto} em ${tentativas} tentativas`
+    );
+    break;
+  } else {
+    if (numeroEscolhido > numeroSecreto) {
+      alert(`O número secreto é menor que ${numeroEscolhido}`);
+    } else {
+      alert(`O número secreto é maior que ${numeroEscolhido}`);
+    }
+    tentativas++;
+  }
 }
