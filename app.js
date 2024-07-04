c4ef11e2a02fda1fbfaa96e411fa0601893660f2
guilhermeuliano <guilherme_uliano@hotmail.com>
2024-07-04 16:17:52 -0300

    Lógica de programação

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Jogo do número secreto!");
+
+let numeroSecreto = 5;
+
+let numeroEscolhido = prompt("Escolha um número de 1 á 10: ");
+
+if (numeroEscolhido == numeroSecreto) {
+  alert("Você acertou");
+}
