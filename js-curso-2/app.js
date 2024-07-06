c7e4858f9222680df65a3454b6f5e9a95bd41b1c
guilhermeuliano <guilherme_uliano@hotmail.com>
2024-07-06 16:44:37 -0300

    Curso 2

---
+++ js-curso-2/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo: número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre # e #';
+
+function verificarChute() {
+  console.log('Teste botão');
+}
